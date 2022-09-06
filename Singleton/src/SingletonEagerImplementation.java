d2d8e6713b17a067b5759e36fd8d4c0155a7609b
DhiviR <rubandhivi8@gmail.com>
2022-09-06 08:59:51 +0530

    Singleton eager implementation

---
+++ Singleton/src/SingletonEagerImplementation.java
@@ -0,0 +1,9 @@
+public class SingletonEagerImplementation {
+    private static final SingletonEagerImplementation instance = new SingletonEagerImplementation();
+
+    private SingletonEagerImplementation() {}
+
+    public static SingletonEagerImplementation getInstance() {
+        return instance;
+    }
+}
