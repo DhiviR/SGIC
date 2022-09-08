e774d5d61817cafea00a7539e406c7623d4428f7
DhiviR <rubandhivi8@gmail.com>
2022-09-08 20:35:11 +0530

    Collections Framework comparison

--- Design patterns/Prototype/src/app/Demo.java
+++ Design patterns/Prototype/src/app/Demo.java
@@ -13,14 +13,9 @@ public class Demo {
         List<Shape> shapesCopy = new ArrayList<>();
 
         Circle circle = new Circle();
-        circle.x = 10;
-        circle.y = 20;
         circle.radius = 15;
         circle.color = "red";
         shapes.add(circle);
-
-        Circle anotherCircle = (Circle) circle.clone();
-        shapes.add(anotherCircle);
 
         Rectangle rectangle = new Rectangle();
         rectangle.width = 10;
@@ -37,6 +32,10 @@ public class Demo {
         }
 
         for (int i = 0; i < shapes.size(); i++) {
+            System.out.println(shapes.get(i));
+            System.out.println(shapesCopy.get(i));
+
+
             if (shapes.get(i) != shapesCopy.get(i)) {
                 System.out.println(i + ": Shapes are different objects (yay!)");
                 if (shapes.get(i).equals(shapesCopy.get(i))) {
