92371d884faea43b2e69365922c2d23145ae5d54
DhiviR <rubandhivi8@gmail.com>
2022-09-12 08:16:20 +0530

    Improved type inference

--- Exercises/Java8NewFeatures/ImprovedTypeInference.java
+++ Exercises/Java8NewFeatures/ImprovedTypeInference.java
@@ -11,11 +11,16 @@ public class ImprovedTypeInference {
      * Type inference is a Java compiler's ability to determine the type of the arguments by looking at the method
      * invocation and corresponding declaration.
      */
-    {
+
+    public static void main(String[] args) {
+//        Won't work in Java 7
         List<String> stringList = new ArrayList<>();
         stringList.add("A");
         stringList.addAll(Arrays.asList());
+
+//        Works in Java 7
+        List<String> stringList2 = new ArrayList<>();
+        stringList2.add("A");
+        stringList2.addAll(Arrays.<String>asList());
     }
-
-
 }
