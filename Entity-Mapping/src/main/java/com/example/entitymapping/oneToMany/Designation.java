d45c643f487766d1079533e1a436c871679c95f3
DhiviR <rubandhivi8@gmail.com>
2022-10-09 22:07:49 +0530

    Entity Mapping Initial commit

---
+++ Entity-Mapping/src/main/java/com/example/entitymapping/oneToMany/Designation.java
@@ -0,0 +1,25 @@
+package com.example.entitymapping.oneToMany;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.example.entitymapping.oneToOne.Employee;
+
+@Entity
+@Table(name = "designation")
+public class Designation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int des_id;
+    private String name;
+    @OneToMany
+    private List<Employee> employee;
+
+}
